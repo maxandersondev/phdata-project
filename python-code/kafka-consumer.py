5d84597f7a7556a95da7830deb7c78b5ea31ac19
Max Anderson <maxanderson006@gmail.com>
2019-06-29 21:04:29 -0500

    getting apache spark working

--- python-code/kafka-consumer.py
+++ python-code/kafka-consumer.py
@@ -7,7 +7,7 @@ consumer = KafkaConsumer(
      bootstrap_servers=['localhost:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
-     group_id='my-group',
+     group_id='spark-streaming',
      value_deserializer=lambda m: loads(m.decode('utf-8')))
 
 
